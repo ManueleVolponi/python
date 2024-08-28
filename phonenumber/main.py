ecc9b53d2927493244237405ad70d3310257dbf0
ManueleV <75172914+ManueleVolponi@users.noreply.github.com>
2024-08-28 18:43:23 +0200

    Update main.py

--- phonenumber/main.py
+++ phonenumber/main.py
@@ -1,6 +1,6 @@
 import phonenumbers
 
 from phonenumbers import geocoder
-phone = phonenumbers.parse("+13108900944")
+phone = phonenumbers.parse("phne_number")
 
 print(geocoder.description_for_number(phone, "en"))
